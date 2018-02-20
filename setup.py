286d19690be1168908110164a0884480ada15f7a
YukunVVan <31962749+YukunVVan@users.noreply.github.com>
2018-02-20 15:09:26 -0500

    init2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='geo-street-talk',
       version='0.1',
       description='Geo-street-talk',
-      url='https://github.com/YukunVVan/geo-street-talk/geo-street-talk',
+      url='https://github.com/YukunVVan/geo-street-talk/',
       author='Yukun Wan',
       author_email='yw3447@nyu.edu',
       license='MIT',
